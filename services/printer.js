1a4b0dd26dc8638210a4bbb6109cdbcd4cca8cbc
Jose Miguel Lueiza Valenzuela <jose.lueiza@lan.com>
2017-06-10 23:20:57 -0400

    new printer url

--- services/printer.js
+++ services/printer.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 let urlPrinter = 'http://localhost:3002';
 const sendQuotes = '/sendQuotes';
 if(process.env.NODE_ENV === 'production') {
-  urlPrinter = 'https://printer-nmqcpkanyq.now.sh';
+  urlPrinter = 'https://printer-frrbczbapx.now.sh';
 }
 module.exports = {
   sendQuotes: (data) => {
